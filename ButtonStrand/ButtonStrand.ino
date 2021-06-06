823f96828009f319bca4012bd897062b9ebb1ad7
Tingliang Zhang ROG <32335780+TingliangZhang@users.noreply.github.com>
2021-06-07 05:49:13 +0800

    increase brightness

--- ButtonStrand/ButtonStrand.ino
+++ ButtonStrand/ButtonStrand.ino
@@ -64,7 +64,7 @@ void setup() {
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 
